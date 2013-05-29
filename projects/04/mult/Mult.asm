62cfc86927b1da2df0d62e0a9bc88ccdfed2d316
nham <hamann.nick@gmail.com>
2013-05-28 22:52:24 -0400

    Add Mult program for chapter 4. I couldnt get the testng to come out right, but it failed in a confusing manner (on the first line, where it prints the headers of values. so im ignoring it for now

--- projects/04/mult/Mult.asm
+++ projects/04/mult/Mult.asm
@@ -7,3 +7,43 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[3], respectively.)
 
 // Put your code here.
+
+
+// set RAM[2] to 0
+@2
+M = 0
+
+// load RAM[0] into D
+@0
+D=M
+
+// if RAM[0] = 0, we can't multiply. so done
+@END
+D;JEQ
+
+// if RAM[0] > 0, don't need to make it positive
+@LOOP
+D;JGT
+
+@1
+M=-M
+@0
+M=-M
+
+(LOOP)
+    // Add RAM[1] to RAM[2]
+    @1
+    D=M
+    @2
+    M=D+M
+
+    // Decrement RAM[0]
+    @0
+    M=M-1
+
+    D=M
+    @LOOP
+    D;JGT
+
+
+(END)

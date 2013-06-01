d9f292bd145f2cb77cdecae5eee6c2381aab300a
nham <hamann.nick@gmail.com>
2013-06-01 18:34:40 -0400

    Add translate function that can translate SimpleAdd into assembly. Passes tests.

--- projects/07/HVMT.py
+++ projects/07/HVMT.py
@@ -1,5 +1,23 @@
 import sys, os
 
+class TranslatorException(Exception):
+    pass
+
+arith_commands = ['add', 'sub', 'neg', 'eq',
+                  'gt', 'lt', 'and', 'or', 'not']
+
+memaccess_commands = ['push', 'pop']
+
+segments = {
+    'argument': 0,
+    'local': 0,
+    'static': 0,
+    'constant': 0,
+    'this': 0,
+    'that': 0,
+    'pointer': 0,
+    'temp': 0}
+
 def tokenize_line(line):
     tokens = []
     currtok = ''
@@ -27,6 +45,34 @@ def read_and_tokenize(fname):
     return commands
 
 
+def translate(command):
+    tokoff = 0
+    pushDtostack = ['@SP', 'A=M', 'M=D', 'D=A+1', '@SP', 'M=D']
+    popstacktoD  = ['@SP', 'D=M', 'AM=D-1', 'D=M']
+
+    if len(command) > 3:
+        raise TranslatorException('Invalid command: too long')
+
+    if command[0] == 'push' and command[1] == 'constant':
+        if (
+            command[2].isdigit() and 
+            int(command[2]) >= 0 and 
+            int(command[2]) <= 32767
+           ):
+            return ['@'+command[2], 'D=A'] + pushDtostack
+        else:
+            raise TranslatorException('Invalid constant')
+    elif command[0] == 'add' and len(command) == 1:
+        # maybe we should push D=D+m and then pushDtostack
+        return popstacktoD + ['A=A-1', 'M=D+M', 'D=A+1', '@SP', 'M=D']
+    else:
+        raise TranslatorException('Unimplemented or invalid!')
+
+
+
+def assembly_init():
+    return ['@256', 'D=A', '@SP', 'M=D']
+
 def doTheThings(path):
     fext = lambda s: s[s.index('.'):]
     # path is either a file name or a directory name
@@ -34,19 +80,34 @@ def doTheThings(path):
         ext = fext(path)
         if  ext == '.vm':
             commands = read_and_tokenize(path)
-            print(path)
-            print('--------------')
-            print(commands)
         else:
             raise TranslatorException('Unrecognized file extension "'+ext+'"')
     else:
-        # drectory
+        isdir = True
+        commands = []
         for fname in os.listdir(path):
             if fext(fname) == '.vm':
-                commands = read_and_tokenize(path+fname)
-                print(path)
-                print('--------------')
-                print(commands)
+                commands += read_and_tokenize(path+fname)
+
+    if len(commands) > 0:
+        assem_out = assembly_init()
+        print(path)
+        print('--------------')
+
+        for cmd in commands:
+            assem_out += translate(cmd)
+            print(cmd)
+            print(assem_out)
+            print('--------------')
+
+            
+        f = open(path[:path.index('.')] + '.asm', 'w')
+        for ass in assem_out:
+            f.write(ass + '\n')
+
+        f.close()
+                
+
 
 if __name__ == "__main__":
     if len(sys.argv) == 2:

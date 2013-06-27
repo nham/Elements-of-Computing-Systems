3d77d92d384e2680d1cafd4c1f56991eacd37bea
nham <hamann.nick@gmail.com>
2013-06-27 21:25:44 +0000

    checkpoint commit for go-based HVMT: add some of the arithmetic command translation logic

--- projects/HVMT.go
+++ projects/HVMT.go
@@ -20,7 +20,8 @@ const (
     C_CALL int = iota
 )
 
-var popstacktoD []string  = []string{"@SP", "D=M", "AM=D-1", "D=M"]
+var popstacktoD []string = []string{"@SP", "D=M", "AM=D-1", "D=M"}
+var pushDtostack []string = []string{"@SP", "A=M", "M=D", "D=A+1", "@SP", "M=D"}
 
 func tokenizeLine(line string) []string {
 	end := len(line) - 1
@@ -61,12 +62,12 @@ func translateCommand(command []string) (string, bool) {
     asm := ""
     switch typeofCommand(command) {
         case C_ARITHMETIC:
-            asm = translateArith(cmd)
+            asm = translateArith(command[0])
         case C_PUSH:
             fallthrough
         case C_POP:
             // not sure if works :)
-            asm = translatePushPop(command...)
+            asm = translatePushPop(command[0], command[1], command[2])
         case C_LABEL:
             asm = translateLabel(command[1])
         case C_GOTO:
@@ -86,11 +87,76 @@ func translateLabel(symbol string) string {
 }
 
 func translateIf(symbol string) string {
-    return append(popstacktoD, []string{"@"+symbol, "D;JNE"}...)
+    cmds := append(popstacktoD, "@"+symbol, "D;JNE")
+    return strings.Join(cmds, "\n")
 }
 
 func translateGoto(symbol string) string {
-    return []string{"@"+symbol, "0;JMP"}
+    return strings.Join([]string{"@"+symbol, "0;JMP"}, "\n")
+}
+
+func translateArith(command string) string {
+    switch command {
+        case "add":
+            fallthrough
+        case "sub":
+            fallthrough
+        case "and":
+            fallthrough
+        case "or":
+            return translateArithBinary(command)
+        case "eq":
+            fallthrough
+        case "lt":
+            fallthrough
+        case "gt":
+            return translateArithCompare(command)
+        case "neg":
+            fallthrough
+        case "not":
+            return translateArithUnary(command)
+        default:
+            return ""
+    }
+}
+
+
+func translateArithBinary(command string) string {
+    lookup := map[string]string{
+        "add": "D+M",
+        "sub": "M-D",
+        "and": "D&M",
+        "or" : "D|M",
+    }
+
+    return strings.Join(
+        append(popstacktoD, "A=A-1", "M="+lookup[command], "D=A+1", "@SP", "M=D"),
+        "\n")
+}
+
+func translateArithCompare(command string) string {
+    /*
+            lab1 = '$LAB'+str(self.labelcount)
+            lab2 = '$LAB'+str(self.labelcount+1)
+            self.labelcount += 2
+
+            return (popstacktoD + ['@R13', 'M=D'] + popstacktoD 
+                    + ['@R13', 'D=D-M', '@'+lab1, 'D;J'+cmd.upper(), 'D=0']
+                    + pushDtostack + ['@'+lab2, '0;JMP', '('+lab1+')', 'D=-1']
+                    + pushDtostack + ['('+lab2+')'])
+                    */
+    return ""
+}
+
+func translateArithUnary(command string) string {
+    lookup := map[string]string{
+        "neg": "!D",
+        "not": "-D",
+    }
+
+    return strings.Join(
+        append(append(popstacktoD, "D="+lookup[command]), pushDtostack...),
+        "\n")
 }
 
 func translatePushPop(command, segment, index string) string {
@@ -106,7 +172,6 @@ func main() {
 		writer := bufio.NewWriter(fo)
 
         cmdtoks := nand2tetris_lib.ReadAndTokenize(path, tokenizeLine)
-        var cmd []string
         for i := range cmdtoks {
             fmt.Println(cmdtoks[i], typeofCommand(cmdtoks[i]))
             asm, success := translateCommand(cmdtoks[i])

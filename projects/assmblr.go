3e291c5a79f9f4ede6a3e4cd410ca4ab1a5dfc93
nham <hamann.nick@gmail.com>
2013-07-04 19:46:09 +0000

    Move functions/variables around in assmblr.go

--- projects/assmblr.go
+++ projects/assmblr.go
@@ -62,6 +62,66 @@ var jumpLookup map[string]string = map[string]string{
 	"JMP":  "111",
 }
 
+const (
+	A_COMMAND int = iota
+	C_COMMAND
+	L_COMMAND
+)
+
+type HackCommand struct {
+	cmdType int
+	tokens  []string
+}
+
+
+type SymbolTable struct {
+	table map[string]uint
+}
+
+func (st *SymbolTable) setEntry(symbol string, address uint) {
+	st.table[symbol] = address
+}
+
+func (st *SymbolTable) getAddress(symbol string) uint {
+	return st.table[symbol]
+}
+
+func (st *SymbolTable) contains(symbol string) bool {
+	_, ok := st.table[symbol]
+	return ok
+}
+
+func initSymbolTable() SymbolTable {
+	return SymbolTable{
+		table: map[string]uint{
+			"SP":     0,
+			"LCL":    1,
+			"ARG":    2,
+			"THIS":   3,
+			"THAT":   4,
+			"R0":     0,
+			"R1":     1,
+			"R2":     2,
+			"R3":     3,
+			"R4":     4,
+			"R5":     5,
+			"R6":     6,
+			"R7":     7,
+			"R8":     8,
+			"R9":     9,
+			"R10":    10,
+			"R11":    11,
+			"R12":    12,
+			"R13":    13,
+			"R14":    14,
+			"R15":    15,
+			"SCREEN": 16384,
+			"kbd":    24576,
+		},
+	}
+}
+
+
 func C_CMDToBin(hc *HackCommand) string {
 	dest, comp, jump := "", "", ""
 	compOff := 0
@@ -113,52 +173,6 @@ func A_CMDToBin(hc *HackCommand, st *SymbolTable, counter func() int) string {
 	return fmt.Sprintf("%016v", strconv.FormatInt(i, 2))
 }
 
-type SymbolTable struct {
-	table map[string]uint
-}
-
-func (st *SymbolTable) setEntry(symbol string, address uint) {
-	st.table[symbol] = address
-}
-
-func (st *SymbolTable) getAddress(symbol string) uint {
-	return st.table[symbol]
-}
-
-func (st *SymbolTable) contains(symbol string) bool {
-	_, ok := st.table[symbol]
-	return ok
-}
-
-func initSymbolTable() SymbolTable {
-	return SymbolTable{
-		table: map[string]uint{
-			"SP":     0,
-			"LCL":    1,
-			"ARG":    2,
-			"THIS":   3,
-			"THAT":   4,
-			"R0":     0,
-			"R1":     1,
-			"R2":     2,
-			"R3":     3,
-			"R4":     4,
-			"R5":     5,
-			"R6":     6,
-			"R7":     7,
-			"R8":     8,
-			"R9":     9,
-			"R10":    10,
-			"R11":    11,
-			"R12":    12,
-			"R13":    13,
-			"R14":    14,
-			"R15":    15,
-			"SCREEN": 16384,
-			"kbd":    24576,
-		},
-	}
-}
 
 func tokenizeLine(line string) []string {
 	punct := [...]string{"@", "=", ";", "(", ")"}
@@ -210,17 +224,6 @@ func tokenizeLine(line string) []string {
 
 	return tokens
 
-}
-
-const (
-	A_COMMAND int = iota
-	C_COMMAND int = iota
-	L_COMMAND int = iota
-)
-
-type HackCommand struct {
-	cmdType int
-	tokens  []string
 }
 
 func createHC(tokens []string) *HackCommand {
